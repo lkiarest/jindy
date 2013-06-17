1e0f9ef4f4cfc80322cf78dea325826c7d3582da
lkiarest <rushi_wowen@163.com>
2013-06-17 22:55:45 +0800

    remove afinal library, use HttpClient instead

--- src/com/qintx/myjindy/content/manager/ArticleService.java
+++ src/com/qintx/myjindy/content/manager/ArticleService.java
@@ -10,14 +10,13 @@ import org.jsoup.select.Elements;
 import com.qintx.myjindy.constant.Constants;
 import com.qintx.myjindy.content.entity.Article;
 import com.qintx.myjindy.http.HttpUtil;
-import com.qintx.myjindy.http.HttpUtil.HttpCallback;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
-public class ArticleService extends Service implements HttpCallback {
+public class ArticleService extends Service {
 
     private static final String TAG = "ArticleService";
     private ArticleManager mgr = ArticleManager.getInstance();
@@ -37,15 +36,23 @@ public class ArticleService extends Service implements HttpCallback {
         String action = intent.getAction();
         Log.d(TAG, "start command : " + action);
         if (Constants.ACTION_GET_ARTICLES.equals(action)) {
-            String channelUrl = intent.getStringExtra(Constants.INTENT_CHANNEL_URL);
+            final String channelUrl = intent.getStringExtra(Constants.INTENT_CHANNEL_URL);
             Log.d(TAG, "channelUrl is  : " + channelUrl);
             if ((channelUrl != null) && (!channelUrl.isEmpty())) {
-                requestArticles(channelUrl);
+                new Thread() {
+                    public void run() {
+                        requestArticles(channelUrl);
+                    }
+                }.start();
             }
         } else if (Constants.ACTION_GET_ARTICLE_CONTENT.equals(action)) {
-            String articalUrl = intent.getStringExtra(Constants.INTENT_ARTICLE_URL);
+            final String articalUrl = intent.getStringExtra(Constants.INTENT_ARTICLE_URL);
             if ((articalUrl != null) && (!articalUrl.isEmpty())) {
-                requestArticleContent(articalUrl);
+                new Thread() {
+                    public void run() {
+                        requestArticleContent(articalUrl);
+                    }
+                }.start();
             }
         }
 
@@ -54,12 +61,34 @@ public class ArticleService extends Service implements HttpCallback {
 
     private void requestArticleContent(final String articleUrl) {
         HttpUtil client = new HttpUtil();
-        client.ajaxGet(articleUrl, new ArticleCallback());
+        String content = client.getContentAsString(articleUrl);
+        Intent intent = new Intent(Constants.ACTION_GET_ARTICLE_CONTENT_RESULT);
+        if (content != null) {
+            addArticle(articleUrl, filterArticleContent(content));
+            intent.putExtra(Constants.ACTION_RESULT, true);
+            intent.putExtra(Constants.INTENT_ARTICLE_URL, articleUrl);
+            sendBroadcast(intent);
+        } else {
+            intent.putExtra(Constants.ACTION_RESULT, false);
+            intent.putExtra(Constants.INTENT_ARTICLE_URL, articleUrl);
+            sendBroadcast(intent);
+        }
     }
 
     private void requestArticles(String channelUrl) {
         HttpUtil client = new HttpUtil();
-        client.ajaxGet(channelUrl, this);
+        String content = client.getContentAsString(channelUrl);
+        Intent intent = new Intent(Constants.ACTION_GET_ARTICLES_RESULT);
+        if (content != null) {
+            managerArticles(channelUrl, content);
+            intent.putExtra(Constants.ACTION_RESULT, true);
+            intent.putExtra(Constants.INTENT_CHANNEL_URL, channelUrl);
+            this.sendBroadcast(intent);
+        } else {
+            intent.putExtra(Constants.ACTION_RESULT, false);
+            intent.putExtra(Constants.INTENT_CHANNEL_URL, channelUrl);
+            this.sendBroadcast(intent);
+        }
     }
 
     private List<Article> filterArticleList(String html) {
@@ -111,38 +140,5 @@ public class ArticleService extends Service implements HttpCallback {
         article.setUrl(url);
         article.setContent(msg);
         mgr.addArticle(url, article);
-    }
-
-    @Override
-    public void onCallback(boolean success, String url, String msg) {
-        Intent intent = new Intent(Constants.ACTION_GET_ARTICLES_RESULT);
-        if (success) {
-            managerArticles(url, msg);
-            intent.putExtra(Constants.ACTION_RESULT, true);
-            intent.putExtra(Constants.INTENT_CHANNEL_URL, url);
-            this.sendBroadcast(intent);
-        } else {
-            intent.putExtra(Constants.ACTION_RESULT, false);
-            intent.putExtra(Constants.INTENT_CHANNEL_URL, url);
-            this.sendBroadcast(intent);
-        }
-    }
-
-    private class ArticleCallback implements HttpCallback {
-
-        @Override
-        public void onCallback(boolean success, String url, String msg) {
-            Intent intent = new Intent(Constants.ACTION_GET_ARTICLE_CONTENT_RESULT);
-            if (success) {
-                addArticle(url, filterArticleContent(msg));
-                intent.putExtra(Constants.ACTION_RESULT, true);
-                intent.putExtra(Constants.INTENT_ARTICLE_URL, url);
-                sendBroadcast(intent);
-            } else {
-                intent.putExtra(Constants.ACTION_RESULT, false);
-                intent.putExtra(Constants.INTENT_ARTICLE_URL, url);
-                sendBroadcast(intent);
-            }
-        }
     }
 }
--- src/com/qintx/myjindy/http/HttpUtil.java
+++ src/com/qintx/myjindy/http/HttpUtil.java
@@ -1,18 +1,86 @@
 package com.qintx.myjindy.http;
 
-import net.tsz.afinal.FinalHttp;
-import net.tsz.afinal.http.AjaxCallBack;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.client.params.CookiePolicy;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 public class HttpUtil {
 
-    private final static HttpClient client = new DefaultHttpClient();
+    private HttpClient client = null;
 
-    public static HttpClient getHttpClient() {
+    public HttpUtil() {
+        client = new DefaultHttpClient();
+    }
+
+    public byte[] getContentAsBytes(String url) {
+        return getContent(url, new ResponseConverter<byte[]>() {
+            public byte[] convert(HttpResponse response) throws IllegalStateException, IOException {
+                return EntityUtils.toByteArray(response.getEntity());
+            }
+        });
+    }
+
+    public String getContentAsString(String url) {
+        return getContent(url, new ResponseConverter<String>() {
+            public String convert(HttpResponse response) throws IllegalStateException, IOException {
+                String content = EntityUtils.toString(response.getEntity());
+                return new String(content.getBytes("ISO-8859-1"), "UTF-8");// for Chinese words
+            }
+        });
+    }
+
+    public InputStream getContentAsStream(String url) {
+        return getContent(url, new ResponseConverter<InputStream>() {
+            public InputStream convert(HttpResponse response) throws IllegalStateException, IOException {
+                return response.getEntity().getContent();
+            }
+        });
+    }
+
+    private <T> T getContent(String url, ResponseConverter<T> converter) {
+        HttpGet method = getHttpGet(url);
+        try {
+            HttpResponse response = client.execute(method);
+            if (response.getEntity() == null) {
+                return null;
+            }
+            return converter.convert(response);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private HttpGet getHttpGet(String url) {
+        HttpGet method = new HttpGet(url);
+        method.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BROWSER_COMPATIBILITY);
+        return method;
+    }
+
+    private class ResponseConverter<T> {
+        public T convert(HttpResponse response) throws IllegalStateException, IOException {
+            return null;
+        }
+    }
+
+    public HttpClient getHttpClient() {
         return client;
     }
+
 /*
     public static String get(String url) {
         HttpGet  method = new HttpGet(url);
@@ -31,56 +99,52 @@ public class HttpUtil {
         
     }
 */
-    private FinalHttp fh;
-
-    public HttpUtil() {
-        fh = new FinalHttp();
-    }
-
-    public void ajaxPost(final String url, final HttpCallback callback) {
-        fh.post(url, new AjaxCallBack<String>() {
-
-            @Override
-            public void onFailure(Throwable t, String strMsg) {
-                if (callback != null) {
-                    callback.onCallback(false, url, strMsg);
-                }
-                super.onFailure(t, strMsg);
-            }
-
-            @Override
-            public void onSuccess(String t) {
-                if (callback != null) {
-                    callback.onCallback(true, url, t);
-                }
-                super.onSuccess(t);
-            }
-        });
-    }
-
-    public void ajaxGet(final String url, final HttpCallback callback) {
-        fh.get(url, new AjaxCallBack<String>() {
-
-            @Override
-            public void onFailure(Throwable t, String strMsg) {
-                if (callback != null) {
-                    callback.onCallback(false, url, strMsg);
-                }
-                super.onFailure(t, strMsg);
-            }
-
-            @Override
-            public void onSuccess(String t) {
-                if (callback != null) {
-                    callback.onCallback(true, url, t);
-                }
-
-                super.onSuccess(t);
-            }
-        });
-    }
-
-    public interface HttpCallback {
-        public void onCallback(boolean success, String url, String msg);
-    }
+//    private FinalHttp fh;
+//
+//    public void ajaxPost(final String url, final HttpCallback callback) {
+//        fh.post(url, new AjaxCallBack<String>() {
+//
+//            @Override
+//            public void onFailure(Throwable t, String strMsg) {
+//                if (callback != null) {
+//                    callback.onCallback(false, url, strMsg);
+//                }
+//                super.onFailure(t, strMsg);
+//            }
+//
+//            @Override
+//            public void onSuccess(String t) {
+//                if (callback != null) {
+//                    callback.onCallback(true, url, t);
+//                }
+//                super.onSuccess(t);
+//            }
+//        });
+//    }
+//
+//    public void ajaxGet(final String url, final HttpCallback callback) {
+//        fh.get(url, new AjaxCallBack<String>() {
+//
+//            @Override
+//            public void onFailure(Throwable t, String strMsg) {
+//                if (callback != null) {
+//                    callback.onCallback(false, url, strMsg);
+//                }
+//                super.onFailure(t, strMsg);
+//            }
+//
+//            @Override
+//            public void onSuccess(String t) {
+//                if (callback != null) {
+//                    callback.onCallback(true, url, t);
+//                }
+//
+//                super.onSuccess(t);
+//            }
+//        });
+//    }
+//
+//    public interface HttpCallback {
+//        public void onCallback(boolean success, String url, String msg);
+//    }
 }
